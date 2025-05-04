22f675bcefe254dd9725a46edc16316ae677c788
Talonachris <chris@pell.cloud>
2025-05-04 02:19:49 +0200

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function switchTab(tab) {
-  const frame = document.getElementById('appFrame');
-  if (tab === 'panel') frame.src = 'https://panel.nebuliton.io';
-  else if (tab === 'shop') frame.src = 'https://nebuliton.io';
-  else if (tab === 'info') frame.src = 'https://nebuliton.de/appinfo';
-}
