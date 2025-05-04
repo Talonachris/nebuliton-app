5459acb8a9d5c3aae61dd39622a7fa4f086b2d11
Talonachris <chris@pell.cloud>
2025-05-04 05:48:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const urls = {
   billing: "https://billing.nebuliton.io"
 };
 
+let lastTab = null;
+
 function switchTab(tab) {
   const frame = document.getElementById("appFrame");
   const loginHint = document.getElementById("login-hint");
   const loginBtn = document.getElementById("loginBtn");
+  const confirmBtn = document.getElementById("confirmLoginBtn");
 
   const target = urls[tab];
+  lastTab = tab;
+
   frame.src = target;
   frame.style.display = "block";
   loginHint.style.display = "none";
@@ -25,7 +30,15 @@ function switchTab(tab) {
       } catch (e) {
         frame.style.display = "none";
         loginHint.style.display = "block";
-        loginBtn.onclick = () => window.open(target, "_blank");
+
+        loginBtn.onclick = () => {
+          window.open(target, "_blank");
+          confirmBtn.style.display = "inline-block";
+        };
+
+        confirmBtn.onclick = () => {
+          switchTab(lastTab); // versuche erneut zu laden
+        };
       }
     }, 2000);
   }
